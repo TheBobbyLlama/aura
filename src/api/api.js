6f740e64ef2c2ff9faf7d4ac086ddd108f301a60
Bob Feuerstein <thebobbyllama@yahoo.com>
2022-04-21 18:59:32 -0500

    Added dummy API for development mockups.

---
+++ src/api/api.js
@@ -0,0 +1,7 @@
+// Dummy API call for development use - eventually this file will be used to communicate with the backend.
+// Send the desired response as a parameter and it will be returned after a delay.
+export const apiCall = (response) => {
+	return new Promise((resolve, ) => {
+		setTimeout(() => { resolve(response) }, 500);
+	});
+}
