8056a75133a4a40a3bb8499a8cb4e9193d9302d6
Bob Feuerstein <thebobbyllama@yahoo.com>
2022-04-27 12:09:02 -0500

    Removed unused imports.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 
 import theme from "./AuraTheme/theme";
