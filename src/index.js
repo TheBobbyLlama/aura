4bf1596bd02e2ad1b6873ae1ab55508e6f356a34
jenni <jenniferli627@gmail.com>
2022-05-06 12:32:40 -0400

    index confilx

--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import { store } from "./redux/store";
 import App from "./App";
 import Login from "./components/Login/LoginPage";
 import Reset from "./components/Reset/Reset";
-<<<<<<< HEAD
-=======
 import Dashboard from "./components/Dashboard/Dashboard";
->>>>>>> 3e9cbdf75d29ead5c284a79f432fce2f2f9f67e5
 
 import "./index.scss";
 
@@ -22,10 +19,7 @@ ReactDOM.render(
           <Route path="/" element={<App />}>
             <Route path="" element={<Login />} />
             <Route path="reset" element={<Reset />} />
-<<<<<<< HEAD
-=======
             <Route path="dashboard" element={<Dashboard />} />
->>>>>>> 3e9cbdf75d29ead5c284a79f432fce2f2f9f67e5
           </Route>
         </Routes>
       </BrowserRouter>
